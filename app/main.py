905896091f05ff7fc2e0a77e0ff9bb6012f406ea
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-13 22:34:38 -0300

    feat: add the error exit

--- app/main.py
+++ app/main.py
@@ -2,14 +2,18 @@ import sys
 
 
 def main():
-
+    valid_commands = []
+    
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-
+        
         # Wait for user input
         cmd = input()
-        sys.stdout.write("{}: command not found\n".format(cmd))
+        if cmd is not valid_commands:
+            sys.stdout.write("{}: command not found\n".format(cmd))
+            sys.stdout.write("$ exit 0")
+            break
         continue
 
 if __name__ == "__main__":
