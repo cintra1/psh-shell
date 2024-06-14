a3558068a5d85814d3414a719d62ab8b700f3b91
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-13 23:00:21 -0300

    feat: add the error exit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import sys
-
+import os
 
 def main():
     valid_commands = ["echo","exit","type"]
+    PATH = os.environ.get("PATH")
     
     while True:
         sys.stdout.write("$ ")
@@ -10,6 +11,12 @@ def main():
         
         # Wait for user input
         cmd = input()
+
+        cmd_path = None
+        paths = PATH.split(":")
+        for path in paths:
+            if os.path.isfile(f"{path}/{cmd}"):
+                cmd_path = f"{path}/{cmd}"
 
         if cmd == "exit 0":
             exit(0)
@@ -20,6 +27,8 @@ def main():
         if cmd.split()[0] == "type":
             if cmd.split()[1] in valid_commands:
                 sys.stdout.write("{} is a shell builtin\n".format(cmd.split()[1]))
+            elif cmd_path:
+                sys.stdout.write(f"{cmd} is {cmd_path}\n")
             else:
                 sys.stdout.write("{}: not found\n".format(cmd.split()[1]))
 
