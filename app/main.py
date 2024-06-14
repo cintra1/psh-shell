f252dfc85f94bfc95df34e7dba7f59a0e850f846
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-13 22:16:01 -0300

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
 
     # Wait for user input
     input()
