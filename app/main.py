2d9e60bbfeb68e34af8d863788a56c6a5107e415
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-14 10:42:41 -0300

    feat: add external programns

--- app/main.py
+++ app/main.py
@@ -13,6 +13,13 @@ def main():
         # Wait for user input
         cmd = input()
 
+        #searching the command into the path
+        cmd_path = None
+        paths = PATH.split(":")
+        for path in paths:
+            if os.path.isfile(f"{path}/{cmd.split()[1]}"):
+                cmd_path = f"{path}/{cmd.split()[1]}"
+
         if cmd == "exit 0":
             exit(0)
 
@@ -20,14 +27,6 @@ def main():
             sys.stdout.write("{}\n".format(cmd[5:]))
 
         if cmd.split()[0] == "type":
-
-            #searching the command into the path
-            cmd_path = None
-            paths = PATH.split(":")
-            for path in paths:
-                if os.path.isfile(f"{path}/{cmd.split()[1]}"):
-                    cmd_path = f"{path}/{cmd.split()[1]}"
-
             if cmd.split()[1] in valid_commands:
                 sys.stdout.write("{} is a shell builtin\n".format(cmd.split()[1]))
             elif cmd_path:
@@ -35,6 +34,11 @@ def main():
             else:
                 sys.stdout.write("{}: not found\n".format(cmd.split()[1]))
 
+        if cmd.split()[0] in paths:
+            file = [cmd_path, cmd.split()[1]]
+            subprocess.call(file)
+
+            # subprocess.run([cmd.split()[0], cmd.split(' ', 1)[1]])
 
         if cmd.split()[0] not in valid_commands:
             sys.stdout.write("{}: command not found\n".format(cmd))
